6a9954c1aa4c9957f2e2d6e35217ec61cac83869
Claudio Alves <claudiohac@gmail.com>
2022-11-28 20:39:28 -0300

    Produto e associação muitos pra muitos com categoria

--- src/main/java/com/claudio/cursomc/Cursomc2Application.java
+++ src/main/java/com/claudio/cursomc/Cursomc2Application.java
@@ -1,7 +1,6 @@
 //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
 package com.claudio.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +9,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.claudio.cursomc.domain.Categoria;
+import com.claudio.cursomc.domain.Produto;
 import com.claudio.cursomc.repositeries.CategoriaRepository;
+import com.claudio.cursomc.repositeries.ProdutoRepository;
 
 @SpringBootApplication
 public class Cursomc2Application implements CommandLineRunner{
@@ -18,6 +19,9 @@ public class Cursomc2Application implements CommandLineRunner{
 	
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc2Application.class, args);
@@ -29,7 +33,20 @@ public class Cursomc2Application implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 
